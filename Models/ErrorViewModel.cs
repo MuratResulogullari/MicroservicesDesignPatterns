103cf9f4fd694e40009821f7cc5ddfd2816ce353
murat.resulogullari <resuloglumurad@gmail.com>
2023-12-09 12:42:46 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroservicesDesignPatterns.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
